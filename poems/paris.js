8b8005eb946d89d12c248619fef6fefbdfb3981f
Mayorman07 <mayowa.olajide@flutterwavego.com>
2025-10-19 13:25:35 +0100

    completed the venice poem

---
+++ poems/paris.js
@@ -0,0 +1,1 @@
+export const parisPoem = "Paris, a city of light and love, with secrets whispered from the stars above. Cafes and bridges, art in every view, a timeless romance, forever fresh and new.";
