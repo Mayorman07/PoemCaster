12d23dd61a2f2187b16c49552804919b3e58d545
Mayowa Olajide <olajidemayorwa@gmail.com>
2025-10-19 13:25:35 +0100

    completed the venice poem

---
+++ poems/venice.js
@@ -0,0 +1,6 @@
+export const venicePoem = [
+    "Venice, the city of masks and dreams,",
+    "where silent canals reflect the moon's soft gleams.",
+    "Gondolas drift on waters dark and deep,",
+    "while ancient secrets in the shadows sleep."
+  ];
