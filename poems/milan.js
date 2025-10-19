a39acd77d0ecb35b0272f5a7729d8c43cade5b28
Mayowa Olajide <olajidemayorwa@gmail.com>
2025-10-19 13:26:15 +0100

    made updates to the milan poem

---
+++ poems/milan.js
@@ -0,0 +1,6 @@
+export const milanPoem = [
+    "Milan, where fashion's heart beats fast and bold,",
+    "a story in every stone, centuries old.",
+    "From Duomo's spires to the Last Supper's grace,",
+    "a city of art, at an unrelenting pace."
+  ];
