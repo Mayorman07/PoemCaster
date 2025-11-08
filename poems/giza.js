e1ffc684d4bdd9bd4439710e4b7e6e7904b38bba
Mayowa Olajide <olajidemayorwa@gmail.com>
2025-11-08 23:11:12 +0100

    worked on more cities

---
+++ poems/giza.js
@@ -0,0 +1,10 @@
+export const gizaPoem = [
+    "The city calls",
+    "Which two fates will entwine",
+    "Which fates will rise through flies, sand and dust - ",
+    "To touch the top of the pyramid",
+    "Love, selfish and vainglorious",
+    "Love that looks donw on the city in derision",
+    "Love, proud - born from struggle, and forged through the desert's silence",
+    "This is Giza , who dares walk this dust, and not be buried by it "
+  ];
--- poems/venice.js
+++ poems/venice.js
@@ -1,6 +1,14 @@
 export const venicePoem = [
     "Venice, the city of masks and dreams,",
-    "where silent canals reflect the moon's soft gleams.",
-    "Gondolas drift on waters dark and deep,",
-    "while ancient secrets in the shadows sleep."
+    "The morning sun pales in comparison to the radiance of my fair maiden",
+    "Who sat by the cascading waterfall, enthralling dull strays",
+    "Her presence the perfect depiction of zen in Eden's garden",
+    "Da Vinci waited for the moment to capture her ethereal beauty",
+    "Many line like sugar ants for the jewel they cannot possess",
+    "Eager to be conscripted for duty",
+    "Protecting the queen from a mindless game of chess",
+    "Swans hiss at the grace in her step",
+    "Her scent reels in all alike, putting flowers to shame",
+    "At the call of her name, kings flex their biceps",
+    "Seeking the glory of fame",
   ];
