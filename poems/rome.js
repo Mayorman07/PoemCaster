988a8b86978969e55c409416f0182ff2756ed58d
Mayorman07 <mayowa.olajide@flutterwavego.com>
2025-11-08 21:35:48 +0100

    took advice from Beryl

--- poems/rome.js
+++ poems/rome.js
@@ -1,6 +1,18 @@
 export const romePoem = [
-    "Rome, where fashion's heart beats fast and bold,",
-    "a story in every stone, centuries old.",
-    "From Duomo's spires to the Last Supper's grace,",
-    "a city of art, roma victor."
+    "In the city of Rome",
+    "Where all alike roamed",
+    "Cupid rolled that i die a lonely minister",
+    "Treacherous trickster",
+    "All the gods played their hands",
+    "Diswagered for the torment of my weary soul",
+    "My misreable fate , the sport of amusement",
+    "My only ally, the Colosseum",
+    "Pregnant with deafening cheers",
+    "Impartial to my misery",
+    "Held my secret",
+    "Their steps carried the weight of my chosen",
+    "Whose kiss could give life",
+    "But stole mine",
+    "Now im left a hollow being",
+    "Doomed to death"
   ];
