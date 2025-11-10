bce23d67647c44114ed6008399ae3b20d59217f4
Mayorman07 <mayowa.olajide@flutterwavego.com>
2025-11-10 21:14:51 +0100

    slowed down the speech

--- poems/rome.js
+++ poems/rome.js
@@ -2,7 +2,7 @@ export const romePoem = [
     "In the city of Rome",
     "Where all alike roamed",
     "Cupid rolled that i die a lonely minister",
-    "Treacherous trickster",
+    "A Treacherous trickster",
     "All the gods played their hands",
     "Diswagered for the torment of my weary soul",
     "My misreable fate , the sport of amusement",
